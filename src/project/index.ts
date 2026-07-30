782b080b60186c350f399aabd1bb3fd1dd26a94d
jlevine456 <jlevine456@users.noreply.github.com>
2026-07-30 15:28:57 -0400

    Stage project workflow controller in project domain (#160)
    * Stage project workflow controller
    
    * Export project workflow controller

--- src/project/index.ts
+++ src/project/index.ts
@@ -1,3 +1,4 @@
 export { ProjectDialog, type ProjectDialogProps } from "./ProjectDialog";
 export { ProjectOverview, ProjectsRoute, ProjectWorkflowTabs } from "./ProjectViews";
 export { ProjectArtifactsView, ReportsRoute } from "./ProjectArtifactViews";
+export { useProjectWorkflow, type UseProjectWorkflowOptions } from "./controller";
